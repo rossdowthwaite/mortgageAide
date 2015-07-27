696acd48cb2f6062c7c7c82e4e69cdd8c3d6997e
Ross Dowthwaite <ross@pixeldotcreative.com>
2015-07-27 13:42:54 +0100

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options[:host] = 'hidden-springs-5321.herokuapp.com'
 end
